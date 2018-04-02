b40c595f64ad6dd5965fe5edac7a4acceb7b1637
Ariel Ivan Diaz <arielivandiaz@gmail.com>
2018-04-02 08:31:43 +0200

    test

--- src/strategy/greedy.rs
+++ src/strategy/greedy.rs
@@ -14,31 +14,8 @@ impl fmt::Display for Greedy {
     }
 }
 
-impl Strategy for Greedy {
-    fn compute_next_move(&mut self, state: &Configuration) -> Option<Movement> {
-        if state.movements().next().is_some() {
-            // Each player want to minimize the value
-            let mut optimal_value: i8 = 127;
-            let mut optimal_mouvement = state.movements().next();
-            // For each mouv we play it and we observe the value of the game
-            for mov in state.movements() {
-                // Play a game will inverse the players, so skip_play is like a second inversion
-                let new_value = state.play(&mov).skip_play().value();
-                // If we find a better mouve, we update it
-                if new_value < optimal_value {
-                    optimal_mouvement = Some(mov);
-                    optimal_value = new_value;
-                }
-            }
-            // We return the best movement
-            return optimal_mouvement;
-        } else {
-            return None;
-        }
-    }
-}
 
-impl Strategy for Greedy_2 {
+impl Strategy for Greedy {
     fn compute_next_move(&mut self, state: &Configuration) -> Option<Movement> {
         if state.movements().next().is_some() {
             // Each player want to minimize the value
